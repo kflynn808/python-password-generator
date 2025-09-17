dfddfce78289329e0aebe64250e6198990bbd32a
kflynn808 <k.flynn808@proton.me>
2025-09-17 12:26:16 -0700

    Added a tests folder, created ppg_hello_world

---
+++ tests/ppg_hello_world.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import random
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Hello World CLI")
+    parser.add_arguments(
+        "-n", "--name",
+        type=str,
+        help="Name to greet"
+    )
+    args = parser.parse_args()
+    if args.name:
+        print(f"Hello {args.name}!")
+    else:
+        print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+
