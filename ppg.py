9abfa2d859d080c2ff9d3241f7afd3b86acdab1e
kflynn808 <k.flynn808@proton.me>
2025-09-19 10:57:38 -0700

    Updated flags and created getRandomChars()

--- ppg.py
+++ ppg.py
@@ -4,6 +4,29 @@ import random
 import argparse
 import string
 
+lower_list = list(string.ascii_lowercase)
+upper_list = list(string.ascii_uppercase)
+number_list = list(string.digits)
+special_list = list(string.punctuation)
+non_valid = ["'","\"","`",".","," ]
+
+for x in non_valid:
+    while x in special_list:
+        special_list.remove(x)
+
+is_upper = False
+is_number = False
+is_special = False 
+is_random = False
+
+
+def getRandomChars(char_list, char_count):
+
+    new_chars = []
+    for x in range(char_count):
+        new_chars.append(random.choice(char_list))
+    
+    return new_chars
 
 
 def main():
@@ -33,24 +56,56 @@ def main():
         type=int,
         help="Length of the string"
     )
+    parser.add_argument(
+        "-cS", "--count-special",
+        type=int,
+        help="Count of special characters used in the string"
+    )
+    parser.add_argument(
+        "-cC", "--count-capital",
+        type=int,
+        help="Count of capital lettersused in the string"
+    )
+    parser.add_argument(
+        "-cN", "--count-numbers",
+        type=int,
+        help="Count of numbers used in the string"
+    )
+
     length_char = 16
+    special_count = 4
+    capital_count = 4
+    lower_count = 4 # lowercase letters count
+    number_count = 4
+    
     args = parser.parse_args()
-    # creating the characters variable and adding onto it allows us to dynamically
-    # choose what type of characters to use based on the flags passed 
+
     characters = string.ascii_lowercase
     if args.special:
-        characters += string.punctuation
+        is_special = True
     elif args.capitals:
-        characters += string.ascii_uppercase        
+        is_upper = True        
     elif args.numbers:
-        characters += string.digits        
+        is_number = True       
     elif args.random:
-        characters = string.ascii_letters + string.punctuation + string.digits
+        is_random = True
     elif args.length:
         length_char = args.length
+    elif args.count_special:
+        special_count = args.count_special
+    elif args.count_capital:
+        capital_count = args.count_capital
+    elif args.count_numbers:
+        numbers_count = count_numbers
     else:
         parser.print_help()
         return
+
+
+    
+
+
+
     random_string = ''.join(random.choice(characters) for i in range(length_char))    
     print(f"[*] Generated String:> {random_string} ")   
 
